a8173b025ccad15a183db32f651f3e9969d127fb
Mad Bernard <madeline.a.bernard@gmail.com>
2016-02-29 22:14:07 -0800

    Express static serving public files

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var app = express();
 var port = process.env.PORT || 5009;
 
 app.use('/', express.static(__dirname + '/public'));
-// app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function(){
   console.log('Server now listening on port ' + port);
