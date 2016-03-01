f6ea6b86da17a859ab7a387112e867824ee79f2b
Mad Bernard <madeline.a.bernard@gmail.com>
2016-02-29 17:19:24 -0800

    Add .gitignore, package.json, start of server/client structure

---
+++ server.js
@@ -0,0 +1,11 @@
+var express  = require('express');
+
+var app = express();
+var port = process.env.PORT || 5009;
+
+app.use('/', express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function(){
+  console.log('Server now listening on port ' + port);
+});
